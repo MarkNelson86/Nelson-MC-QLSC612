bd9ddb058af214a1a3eb009705d82482ae4c387e
mnelson <mark.nelson3@mail.mcgill.ca>
2020-05-28 15:20:34 -0700

    mod significance testing & modeling
    Found spurious significant result! Just need to visualize...

--- practical/scripts/myanalysis.py
+++ practical/scripts/myanalysis.py
@@ -22,13 +22,14 @@ Created on Thu May 28 07:36:05 2020
 
 # import csv
 import os.path
+
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from scipy import stats as sst
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
-#  import statsmodels.api as sm
+import statsmodels.api as sm
 
 
 ## Read in CSV file to pandas DataFrame & manipulate data
@@ -47,16 +48,17 @@ del BS['Unnamed: 0']                                                            
 
 
 # Add random variable partY to each entry in dataframe
-BS['partY'] = pd.Series(np.random.randn(len(BS)), index=BS.index)
+#BS['partY'] = pd.Series(np.random.randn(len(BS)), index=BS.index)              # continuous random number
+BS['partY'] = pd.Series(np.random.randint(0,10,size=len(BS)), index=BS.index)   # catagorical
 
 # Prepare data by removing rows with missing values and forcing categorical & float dtype
 BSmod = BS.replace('Female', 0)
 BSmod = BSmod.replace('Male', 1)
 miss_vals_rowi = [BSmod[BSmod['Weight'] == '.'].index.values]
-BSmod = BSmod.drop(BSmod.index[BSmod[BSmod['Weight'] == '.'].index.values])     # del rows with missing values
+BSmod = BSmod.drop(BSmod.index[miss_vals_rowi])                                 # del rows with missing values
 BSmod = BSmod.astype('float')
 BSmod['Gender'] = BSmod['Gender'].astype('category')                            # all types = float except gender = categorical
-
+# BSmod['partY'] = BS['partY'].drop(BS.index[miss_vals_rowi])                   # to switch out partY vectors
 
 
 
@@ -70,34 +72,52 @@ BSmod['Gender'] = BSmod['Gender'].astype('category')                            
 col_names = BS.columns.tolist()                                                 # extract column names as list
 X = BS.drop(col_names[-1],1)                                                    # isolate predictors (rm Y values)
 Y = BS[col_names[-1]]
+Y = BSmod[col_names[-1]]
+
 
 # prepare data for model
-BS.dtypes                                                                       # show column datatypes
-X = X.replace('Female', 0)                                                      # replace gender strings with binary
-X = X.replace('Male', 1)
-X['Gender'].astype('category')                                                  # and change to categorical type
-
-X[X == '.']                                                                     # shows location of missing values
-miss_vals_rowi = X[X['Weight'] == '.'].index.values
-X = X.drop(X.index[miss_vals_rowi])                                             # rm rows with missing values
-Y = Y.drop(Y.index[miss_vals_rowi])                                             # from Y vector also
-
-X = X.astype('float')                                                           # changes all values to float type
-# alternatively could change individual column types using: X['Weight'] = X['Weight'].astype('float')
+#BS.dtypes                                                                       # show column datatypes
+#X = X.replace('Female', 0)                                                      # replace gender strings with binary
+#X = X.replace('Male', 1)
+#X['Gender'].astype('category')                                                  # and change to categorical type
+#
+#X[X == '.']                                                                     # shows location of missing values
+#miss_vals_rowi = X[X['Weight'] == '.'].index.values
+#X = X.drop(X.index[miss_vals_rowi])                                             # rm rows with missing values
+#Y = Y.drop(Y.index[miss_vals_rowi])                                             # from Y vector also
+#
+#X = X.astype('float')                                                           # changes all values to float type
+## alternatively could change individual column types using: X['Weight'] = X['Weight'].astype('float')
 
 # model using sklearn.linear_model
 lm = linear_model.LinearRegression()
-model = lm.fit(X,Y)
-predictions = lm.predict(X)                                                     # predicts Y from X using model
+model_skl = lm.fit(X,Y)
+predictions_skl = lm.predict(X)                                                 # predicts Y from X using model
 
 # Print out the statistics
-print(predictions)
+print(predictions_skl)
 lm.score(X,Y)                                                                   # R^2 score of our model
 lm.coef_                                                                        # our model coeffs
 lm.intercept_
 
+# Visualize 
+plt.scatter(X[col_names[0]], Y,  color='black')                                 # plot X columns vs Y individually
+plt.plot(X[col_names[0]], predictions_skl, color='blue', linewidth=3)           # X columns vs predicted Y
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+for p in range(len(col_names)-1):                                               # How to plot multiple plots together??
+    plt.scatter(X[col_names[p]], Y,  color='black')
 
 
+
+## Regression with statsmodels
+
+X = X.astype(float)
+model_sm = sm.OLS(Y.astype(float), X.astype(float)).fit()
+predictions_sm = model_sm.predict(X)                                            # make the predictions by the model
+model_sm.summary()                                                              # Print out the statistics
 
 
 
@@ -111,7 +131,7 @@ male_partY = BS[BS['Gender'] == 'Male']['partY']
 male_vs_female_partY_tstat = sst.ttest_ind(female_partY, male_partY)[0]
 male_vs_female_partY_pval = sst.ttest_ind(female_partY, male_partY)[1]
 
-# 2-sample t-test: positive or negative partY vs other variables: p-hacking
+# 2-sample t-test: Columnwise, all variables, split by + or - partY value: p-hacking
 BSmod_partY_neg = BSmod[BSmod['partY'] < 0]
 BSmod_partY_pos = BSmod[BSmod['partY'] > 0]
 stats_store_sign = []                                                           # store results of tests in nested list
@@ -139,6 +159,73 @@ for i in range(len(stats_store_size)):                                          
     print(stats_store_size[i][1])
     
 
+# 2-sample t-test for binary Y: 0 or 1 partY vs other variables: p-hacking
+BSmod_partY_0 = BSmod[BSmod['partY'] == 0]
+BSmod_partY_1 = BSmod[BSmod['partY'] == 1]
+stats_store_bin = []                                                            # store results of tests in nested list
+
+for col in range(len(col_names)):                                               # loop through columns
+    BSmod_partY_0_tempvals = BSmod_partY_0[col_names[col]]                      # get values for tests
+    BSmod_partY_1_tempvals = BSmod_partY_1[col_names[col]]
+    stats_store_bin.append(sst.ttest_ind(BSmod_partY_0_tempvals, BSmod_partY_1_tempvals)) # tests
+    
+for i in range(len(stats_store_bin)):                                               # print p values
+    print(stats_store_bin[i][1])
+    
+
+
+
+## Definite P-hacking: FIND THE SIGNIFICANT VALUES!!!
+
+# 2-sample t-test: columnwise, by categorical partY value vs remainder, for each unique partY value individually
+BSmod_partY_unique = BSmod.partY.unique()
+BSmod_partY_unique.sort()
+#BSmod_partY_0 = BSmod[BSmod['partY'] == 0]
+#BSmod_partY_1 = BSmod[BSmod['partY'] == 1]
+stats_store_cat_out = []                                                        # store results of tests in nested list
+WINNERS = {}
+
+for val in range(len(BSmod_partY_unique)):                                      # iterate through categorical Y values
+    BSmod_partY_valu = BSmod[BSmod['partY'] == BSmod_partY_unique[val]]
+    BSmod_partY_rest = BSmod[BSmod['partY'] != BSmod_partY_unique[val]]
+    stats_store_cat_in = []
+    
+    for col in range(len(col_names)-1):                                         # loop through columns
+        BSmod_partY_valu_tempvals = BSmod_partY_valu[col_names[col]]            # get values for tests
+        BSmod_partY_rest_tempvals = BSmod_partY_rest[col_names[col]]
+        
+        t, p = sst.ttest_ind(BSmod_partY_valu_tempvals, BSmod_partY_rest_tempvals)
+        stats_store_cat_in.append([t,p])
+        
+        if p <= .05:
+            WINNERS['y_value'] = BSmod_partY_unique[val]
+            WINNERS['x_column'] = col_names[col]                                # Y = 9, X = 'MRI_Count'
+        
+    stats_store_cat_out.append(stats_store_cat_in)
+        
+for i in range(len(stats_store_cat_out)):                                       # print p values
+    
+    for j in range(len(stats_store_cat_out[i])):
+        print(stats_store_cat_out[i][j][1])
+    
+
+## Verify WINNERS
+
+BSmod_partY_9 = BSmod[BSmod['partY'] == 9]
+BSmod_partY_rest = BSmod[BSmod['partY'] != 9]
+
+BSmod_partY_9_tempvals = BSmod_partY_9['MRI_Count']                      # get values for tests
+BSmod_partY_rest_tempvals = BSmod_partY_rest['MRI_Count']
+
+t, p = sst.ttest_ind(BSmod_partY_9_tempvals, BSmod_partY_rest_tempvals)         # p < .039!!!
+
+
+
+## Visualize significant result!
+    
+
+
+## Pairwise correlations
 
 
 
